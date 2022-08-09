b1116e83b21bc9166f59034df77668a7251e288e
Dollat7852 <dollat4real@gmail.com>
2022-08-09 06:20:53 +0100

    work in progress

--- test/main.c
+++ test/main.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 /**
  * main - main function
- * 
- * 
+ *
+ *
  */
-void main(void){
+void main(void)
+{
     char a = 'H';
-    char * b = "Hello World!" ;
+    char *b = "Hello World!";
     int count = 0;
-    int n =0;
+    int n = 0;
     double c = 2.7;
     //_printf("Hello %c %c World %c %s %%\n", a, 'b', 'e', "dayo");
-    n = printf("%s%c%c%d\n","dayonh",a,a, -5674);
-    printf("%s%c%c%i\n","dayonh",a,a, -5674);
+    n = printf("%s\n", "World");
+    //  printf("%s%c%c%i\n","dayonh",a,a, -5674);
     printf("%d\n", n);
-    count = _printf("%s%c%c%i\n","dayonh",a,a, -5674);
-    _printf("%s%c%c%d\n","dayonh",a,a, -5674);
+    count = _printf("%s\n", "World");
+    // _printf("%s%c%c%d\n","dayonh",a,a, -5674);
     printf("%d\n", count);
-    
 }
